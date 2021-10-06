a061bdac8c066202272feaf9a8dc00aa7973a511
reeveboy <reeveboy10@gmail.com>
2021-10-06 09:09:00 +0530

    Message Feature

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,6 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  // States for contact form fields
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  //   Form validation state
+  const [errors, setErrors] = useState({});
+
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  // Validation check method
+  const handleValidation = () => {
+    let tempErrors = {};
+    let isValid = true;
+
+    if (name.length <= 0) {
+      tempErrors["fullname"] = true;
+      isValid = false;
+    }
+    if (email.length <= 0) {
+      tempErrors["email"] = true;
+      isValid = false;
+    }
+    if (message.length <= 0) {
+      tempErrors["message"] = true;
+      isValid = false;
+    }
+
+    setErrors({ ...tempErrors });
+    return isValid;
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  //   Handling form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setSubmitting(true);
+
+    let isValidForm = handleValidation();
+
+    if (isValidForm) {
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+          email: email,
+          name: name,
+          message: message,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      const { error } = await res.json();
+      if (error) {
+        setSubmitting(false);
+        return;
+      }
+    }
+    setSubmitting(false);
+  };
+
   return (
     <div className="flex flex-col bg-white w-80 rounded-lg md:min-w-96 md:w-96 md:min-h-96">
       <div className="flex justify-center items-center bg-new_yellow rounded-t-lg p-2">
@@ -20,25 +94,39 @@ const ContactForm = () => {
         <span className="text-cyan text-tiny md:text-sm">
           Please fill this form in a decent manner
         </span>
-        <form className="flex flex-col justify-between w-full m-2">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-between w-full m-2">
           <div className="flex flex-col text-tiny md:text-base">
             <span className="text-cyan font-semibold">Name</span>
-            <input className="border rounded-sm md:px-2 md:py-1" type="text" />
+            <input
+              onChange={handleNameChange}
+              className="border rounded-sm md:px-2 md:py-1"
+              type="text"
+            />
           </div>
           <div className="flex flex-col text-tiny md:text-base mt-1">
             <span className="text-cyan font-semibold">Email</span>
-            <input className="border rounded-sm md:px-2 md:py-1" type="email" />
+            <input
+              onChange={handleEmailChange}
+              className="border rounded-sm md:px-2 md:py-1"
+              type="email"
+            />
           </div>
           <div className="flex flex-col text-tiny md:text-base mt-1">
             <span className="text-cyan font-semibold">Message</span>
             <textarea
+              onChange={handleMessageChange}
               className="border rounded-sm md:text-sm md:px-2 md:py1"
               rows={2}
               cols={60}
               name="message"></textarea>
           </div>
           <div className="flex justify-center items-center mt-3">
-            <button className="bg-nice_pink px-2 py-1 text-tiny text-white rounded-sm font-semibold md:rounded-md md:py-2 md:px-3 md:text-xs">
+            <button
+              disabled={isSubmitting}
+              onClick={handleSubmit}
+              className="bg-nice_pink px-2 py-1 text-tiny text-white rounded-sm font-semibold md:rounded-md md:py-2 md:px-3 md:text-xs">
               SUBMIT
             </button>
           </div>
---
+++ pages/api/sendgrid.ts
@@ -0,0 +1,44 @@
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendEmail(req, res) {
+  try {
+    await sendgrid.send({
+      to: "barretosweden@gmail.com", // Your email where you'll receive emails
+      from: "reeveboy10@gmail.com", // your website email address here
+      subject: `Message from ${req.body.name}`,
+      html: `
+      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new mail from 🧍${req.body.name}, their email is: ✉️${req.body.email} </h3>
+              <div style="font-size: 16px;">
+              <h5>Message: ${req.body.message}</h5>
+              <br>
+              </div>
+      </body>
+      </html>`,
+    });
+  } catch (error) {
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+  return res.status(200).json({ error: "" });
+}
+
+export default sendEmail;
