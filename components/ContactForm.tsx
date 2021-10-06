b848eb9f98131f9367adcdaab1a6a86b7221f2d6
reeveboy <reeveboy10@gmail.com>
2021-10-06 09:39:10 +0530

    For Success and Fail messages

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -9,6 +9,9 @@ const ContactForm = () => {
   //   Form validation state
   const [errors, setErrors] = useState({});
 
+  const [showSuccess, setSuccess] = useState(false);
+  const [showFail, setFail] = useState(false);
+
   const [isSubmitting, setSubmitting] = useState(false);
 
   // Validation check method
@@ -49,6 +52,9 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setFail(false);
+    setSuccess(false);
+
     setSubmitting(true);
 
     let isValidForm = handleValidation();
@@ -69,7 +75,15 @@ const ContactForm = () => {
       const { error } = await res.json();
       if (error) {
         setSubmitting(false);
+        setFail(true);
         return;
+      }
+
+      if (res.status === 200) {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSuccess(true);
       }
     }
     setSubmitting(false);
@@ -103,6 +117,7 @@ const ContactForm = () => {
               onChange={handleNameChange}
               className="border rounded-sm md:px-2 md:py-1"
               type="text"
+              value={name}
             />
           </div>
           <div className="flex flex-col text-tiny md:text-base mt-1">
@@ -111,6 +126,7 @@ const ContactForm = () => {
               onChange={handleEmailChange}
               className="border rounded-sm md:px-2 md:py-1"
               type="email"
+              value={email}
             />
           </div>
           <div className="flex flex-col text-tiny md:text-base mt-1">
@@ -120,7 +136,9 @@ const ContactForm = () => {
               className="border rounded-sm md:text-sm md:px-2 md:py1"
               rows={2}
               cols={60}
-              name="message"></textarea>
+              name="message"
+              value={message}
+            />
           </div>
           <div className="flex justify-center items-center mt-3">
             <button
@@ -129,6 +147,14 @@ const ContactForm = () => {
               className="bg-nice_pink px-2 py-1 text-tiny text-white rounded-sm font-semibold md:rounded-md md:py-2 md:px-3 md:text-xs">
               SUBMIT
             </button>
+          </div>
+          <div className="text-xs text-center">
+            <span hidden={!showSuccess} className="text-green-400">
+              Your message was sent successfully
+            </span>
+            <span hidden={!showFail} className="text-red-400">
+              Your message did not go through
+            </span>
           </div>
         </form>
       </div>
